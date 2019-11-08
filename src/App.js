f284eebbe8587b2460e3454e5aa0922655449768
Parayre <kevinparayre@mbp-de-parayre.lan>
2019-11-08 15:22:25 +0100

    ajout dans la list done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const newTasks = [...tasks];
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    newTasks.push({ name: taskInput });
+    setTasks(newTasks);
+  };
+
+  console.log("current tasks", tasks);
+
   return (
-    <>
-      <h1>TO do list</h1>
-    </>
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <h1>To-Do list</h1>
+        <ul className="list">
+          {tasks.map((tasks, index) => {
+            return <li key={index}>{tasks.name}</li>;
+          })}
+        </ul>
+        <input
+          className="addBar"
+          placeholder="Title"
+          type="text"
+          value={taskInput}
+          onChange={event => {
+            setTaskInput(event.target.value);
+          }}
+        />
+        <input className="button" type="submit" value="Add Task" />
+      </form>
+    </div>
   );
 };
 
